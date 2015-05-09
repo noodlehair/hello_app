a7dd4f76b119d8c1d036eeba2caa7af78e8fa6f8
noodlehair <lanceton_dsouza@yahoo.co.in>
2015-05-09 02:59:11 -0700

    Routes to a Personalized message instead of the hello world message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def hello
     render text: "hello, world!"
   end
+
+  def person
+    render text: "Hey Marvin! My first Ruby on Rails web app!"
+  end
 end
